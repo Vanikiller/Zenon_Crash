5117ad62b40d836d544e839ed2d7be627084a604
Web-harsh <godwinoloche50@gmail.com>
2024-09-28 14:34:01 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,19 +4,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "48459439078"
-global.ownername = "Toxxic Md"
-global.ytname = "Toxxic Md"
+global.ownernomer = "2348141104435"
+global.ownername = "༆Godwin᭄☆♡ᴬˡᵖʰᵃ"
+global.ytname = "༆Godwin᭄☆♡ᴬˡᵖʰᵃ"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '48459439078'  //creator number
-global.ownername = 'Toxxic Md' //owner name
-global.botname = 'ᴢᴇɴᴏɴ ᴍᴅ' //name of the bot
+global.ownernumber = '2348141104435'  //creator number
+global.ownername = '༆Godwin᭄☆♡ᴬˡᵖʰᵃ' //owner name
+global.botname = 'TÎÑUBU~V11'//name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'ZENON\n\nContact: 48459439078'
+global.author = 'ZENON\n\nContact: 2348141104435'
 
 //console view/theme
 global.themeemoji = '💀'
@@ -29,8 +29,8 @@ global.link = 'https://whatsapp.com/channel/0029Vaex2BtGU3BRQoeEsl0U'
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
 global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
